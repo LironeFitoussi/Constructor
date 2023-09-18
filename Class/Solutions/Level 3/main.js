91afcf201e2aab8157276499bb0e6cb68b83280d
LironeFitoussi <lironefit@icloud.com>
2023-09-18 16:46:37 +0300

    UPDT

--- Class/Solutions/Level 3/main.js
+++ Class/Solutions/Level 3/main.js
@@ -1,20 +1,20 @@
 console.log("test");
-class Calculator{
-    static addition(a, b){
-        return a + b
-    } 
+class Calculator {
+  static addition(a, b) {
+    return a + b;
+  }
 
-    static subtraction(a, b){
-        return a - b
-    }
+  static subtraction(a, b) {
+    return a - b;
+  }
 
-    static multiplication(a, b){
-        return a * b
-    }
+  static multiplication(a, b) {
+    return a * b;
+  }
 
-    static division(a, b) {
-        return a / b
-    }
+  static division(a, b) {
+    return a / b;
+  }
 }
 
 console.log(Calculator.addition(4, 8));
@@ -23,10 +23,9 @@ console.log(Calculator.multiplication(12, 8));
 console.log(Calculator.division(80, 8));
 
 class StringUtils {
-    static reverseString(inputString) {
-      return inputString.split(``).reverse().join(``);
-    }
+  static reverseString(inputString) {
+    return inputString.split(``).reverse().join(``);
   }
-  
-  console.log(StringUtils.reverseString("Hello"));
+}
 
+console.log(StringUtils.reverseString("Hello"));
