81d6c59ddc1d9dfb582aa63839ed5517e65fe11c
LironeFitoussi <lironefit@icloud.com>
2023-09-18 16:48:53 +0300

    UPDT

--- Class/Solutions/Level 3/main.js
+++ Class/Solutions/Level 3/main.js
@@ -1,3 +1,4 @@
+// Exercise 1
 console.log("test");
 class Calculator {
   static addition(a, b) {
@@ -22,6 +23,7 @@ console.log(Calculator.subtraction(12, 8));
 console.log(Calculator.multiplication(12, 8));
 console.log(Calculator.division(80, 8));
 
+// Exercise 2
 class StringUtils {
   static reverseString(inputString) {
     return inputString.split(``).reverse().join(``);
@@ -29,3 +31,27 @@ class StringUtils {
 }
 
 console.log(StringUtils.reverseString("Hello"));
+
+// Exercise 3
+class ArrayUtil {
+    static findMax(arr) {
+      arr.sort(function(a, b) {
+        return a - b;
+      });
+  
+      return arr[arr.length - 1];
+    }
+  
+    static findMin(arr) {
+      arr.sort(function(a, b) {
+        return a - b;
+      });
+  
+      return arr[0];
+    }
+  }
+  
+  const points = [40, 100, 1, 5, 25, 10];
+  console.log("Maximum:", ArrayUtil.findMax(points));
+  console.log("Minimum:", ArrayUtil.findMin(points));
+  
