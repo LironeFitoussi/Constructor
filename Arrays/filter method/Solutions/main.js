6a1d40e0a618d99482f4d1de2402734a5560ecf4
LironeFitoussi <lironefit@icloud.com>
2023-09-19 21:18:31 +0300

    Exercise 3 Done

--- Arrays/filter method/Solutions/main.js
+++ Arrays/filter method/Solutions/main.js
@@ -5,13 +5,17 @@ console.log(numbers1);
 
 // Exercise 2 Solution 1
 let names2 = ["Alice", "Bob", "Charlie", "David", "Eve"];
-names2 = names2.filter((num) => num.indexOf("A") !== 0)
+names2 = names2.filter((name) => name.indexOf("A") !== 0)
 console.log(names2);
 
 // Exercise 2 Solution 2
-names2 = names2.filter((num) => !num.startsWith("A"))
+names2 = names2.filter((name) => !name.startsWith("A"))
 console.log(names2);
 
 // Exercise 3
 let numbers3 = [2, -5, 10, -8, 3, -1];
+numbers3 = numbers3.filter((num) => num > 0)
+console.log(numbers3);
+
+// Exercise 4
 
