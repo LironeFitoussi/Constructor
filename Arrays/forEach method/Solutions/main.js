3f80330f00af487d6bf85b68116bbcf1b2de6aa3
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2023-09-21 08:49:35 +0300

    Exercise 1 + 2 Done

---
+++ Arrays/forEach method/Solutions/main.js
@@ -0,0 +1,17 @@
+console.log("Test");
+
+// Exercise 1
+//? Use the .forEach() method to print each number in the array.
+//! const numbers1 = [1, 2, 3, 4, 5, 6];
+
+const numbers1 = [1, 2, 3, 4, 5, 6];
+
+numbers1.forEach(num => console.log(num));
+
+// Exercise 2
+//? Print the names in the array, one at a time, using .forEach().
+//! const names2 = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+const names2 = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+names2.forEach(name => console.log(name))
