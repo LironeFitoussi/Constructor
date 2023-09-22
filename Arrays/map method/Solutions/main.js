cfc86a11d4171b76dc927cc6586afd67047b694b
LironeFitoussi <lironefit@icloud.com>
2023-09-22 16:25:23 +0300

    Exercise 2 Done

---
+++ Arrays/map method/Solutions/main.js
@@ -0,0 +1,35 @@
+console.log("Test");
+// Exercise 1
+//? Use map to double each element in arr1
+
+const arr1 = [1, 2, 3];
+const doubledArr = arr1.map(num => num*2)
+console.log(doubledArr);
+
+// Exercise 2
+//? Use map to convert each element in arr2 to uppercase
+const arr2 = ['apple', 'banana', 'cherry'];
+const upperCaseArr2 = arr2.map(fruit => fruit.toUpperCase())
+console.log(upperCaseArr2);
+
+
+// Exercise 3
+// Exercise 4
+// Exercise 5
+// Exercise 6
+// Exercise 7
+// Exercise 8
+// Exercise 9
+// Exercise 10
+// Exercise 11
+// Exercise 12
+// Exercise 13
+// Exercise 14
+// Exercise 15
+// Exercise 16
+// Exercise 17
+// Exercise 18
+// Exercise 19
+// Exercise 20
+
+
